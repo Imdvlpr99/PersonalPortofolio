7915f72f97c5426dc0d9c7215a26e14bae803ae8
Agung Jaya <agung.jaya@ist.id>
2024-01-27 19:38:17 +0700

    feat : add home and splash screen

---
+++ lib/screen/splash.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<StatefulWidget> createState() => SplashState();
+}
+
+class SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+
+}
