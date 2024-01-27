17ac4543d03dc11f07445f22642a27ac0863ebbf
Agung Jaya <agung.jaya@ist.id>
2024-01-27 20:53:59 +0700

    feat : add lottie and set splash screen anim

--- lib/screen/splash.dart
+++ lib/screen/splash.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:personal_portofolio/utils/constants.dart';
 
 class Splash extends StatefulWidget {
   const Splash({super.key});
@@ -11,8 +13,14 @@ class SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: primaryBlack,
+      body: Center(
+        child: Lottie.asset(
+          'assets/anim/loading.json',
+          width: 200,
+          height: 200,
+        ),
+      ),
     );
   }
-
 }
