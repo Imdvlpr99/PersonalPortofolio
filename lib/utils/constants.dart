a5559cc63dee29b67986e06d57098189cb57beb7
Imdvlpr99 <imdvlpr.agency@gmail.com>
2024-01-29 23:17:01 +0700

    feat : add tertiaryPurple color

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 const Color mainColor = Color(0xFF7456FF);
 const Color secondaryColor = Color(0xFF8368FF);
+const Color tertiaryPurple = Color(0xFFA693FF);
 const Color primaryBlack = Color(0xFF0C0C0C);
 const Color secondaryBlack = Color(0xFF0F0F0F);
 const Color primaryGray = Color(0xFF707070);
