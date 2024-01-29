3d57e8247c4a9510b32363798aa714b8f7856b4d
Agung Jaya <agung.jaya@ist.id>
2024-01-29 21:46:38 +0700

    feat : create main pages

---
+++ lib/screen/notes.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Notes extends StatefulWidget {
+  const Notes({super.key});
+
+  @override
+  State<StatefulWidget> createState() => NotesState();
+}
+
+class NotesState extends State<Notes> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+
+}
---
+++ lib/screen/projects.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Projects extends StatefulWidget {
+  const Projects({super.key});
+
+  @override
+  State<StatefulWidget> createState() => ProjectsState();
+
+}
+
+class ProjectsState extends State<Projects> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+
+}
